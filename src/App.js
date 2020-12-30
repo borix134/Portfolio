93c22e4efcbb1145197bc3c99cee3a4a662ba1a0
Nick <nick@panagakis.com>
2020-12-29 18:47:26 -0600

    Changed title, removed reduntant body tag.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import './App.css';
 import Panel from './components/Panel';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+     document.title = "Nicholas P"
+  }, []);
+
   return (
     <div className="App">
-      <header>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Sunflower:wght@300&display=swap" rel="stylesheet" />
-        <title>/home/nick/</title>
-      </header>
-      <body>
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Sunflower:wght@300&display=swap" rel="stylesheet" />
         <h2>
-          Nick Panagakis <block>█</block>
+          Nick Panagakis 
         </h2>
         <h3>
-          Web Development
+          Web Development<block>█</block>
         </h3>
         <Panel></Panel>
-      </body>
     </div>
   );
 }
